4b40906520b262a29ee4988c2a3674c7e232431f
Timo Lindhorst <timo.lindhorst@travelping.com>
2017-04-05 10:50:15 +0200

    Some clean-up in docker-entrypoint scripts

--- pgw-c-node/docker-entrypoint.sh
+++ pgw-c-node/docker-entrypoint.sh
@@ -9,39 +9,39 @@ exit_msg () {
 # creates erlang notation of ip address without netmask
 # 192.168.10.1/24 -> 192,168,10,1
 ip_to_erlang() {
-    echo $1 | sed 's/\./,/g' - | sed 's/\/.*$//g' - | sed 's/.*/{\0}/g' -
+	echo $1 | sed 's/\./,/g' - | sed 's/\/.*$//g' - | sed 's/.*/{\0}/g' -
 }
 
 # convert domain name
 # apn.example.net -> [<<"apn">>,<<"example">>,<<"net">>]
 apn_to_erlang () {
-  APN=""
-  IFS='.' read -ra LABEL <<< $1
-  for i in "${LABEL[@]}"; do
-    if [ -z "$APN" ] ; then
-      APN="[<<\"$i\">>"; 
-    else
-      APN+=",<<\"$i\">>"
-    fi
-  done
-  APN+="]"
-  echo $APN
+	APN=""
+	IFS='.' read -ra LABEL <<< $1
+	for i in "${LABEL[@]}"; do
+		if [ -z "$APN" ] ; then
+			APN="[<<\"$i\">>";
+		else
+			APN+=",<<\"$i\">>"
+		fi
+	done
+	APN+="]"
+	echo $APN
 }
 
 parse_generic () {
-  #echo parsing ${1}=${!1}
-  [ -n "${!1}" ] || ( echo "env variable $1 is not set"; false )
+	#echo parsing ${1}=${!1}
+	[ -n "${!1}" ] || ( echo "env variable $1 is not set"; false )
 }
 
 parse_ip_addr () {
-  #TODO: some more validation for IP addresses
-  parse_generic $1 && eval export ${1}_ERL=\"$(ip_to_erlang ${!1})\"
+	#TODO: some more validation for IP addresses
+	parse_generic $1 && eval export ${1}_ERL=\"$(ip_to_erlang ${!1})\"
 }
 
 parse_apn () {
-  parse_generic $1 || return 1
-  printf -v ${1}_ERL "$(apn_to_erlang ${!1})"
-  export ${1}_ERL
+	parse_generic $1 || return 1
+	printf -v ${1}_ERL "$(apn_to_erlang ${!1})"
+	export ${1}_ERL
 }
 
 
@@ -54,18 +54,14 @@ VALIDATION_ERROR=""
 parse_generic PGW_S5C_IFACE || VALIDATION_ERROR=1
 parse_apn PGW_APN || VALIDATION_ERROR=1
 
-PARAMS_IPADDR="
-    PGW_S5C_IPADDR
-    PGW_CLIENT_IPADDR_START
-    PGW_CLIENT_IPADDR_END
-    PGW_CLIENT_PRIMARY_DNS
-    PGW_CLIENT_SECONDARY_DNS
-    PGW_CLIENT_PRIMARY_NBNS
-    PGW_CLIENT_SECONDARY_NBNS
-  "
-for var in $PARAMS_IPADDR; do
-  parse_ip_addr $var || VALIDATION_ERROR=1
-done
+# parse IP addresses
+parse_ip_addr PGW_S5C_IPADDR || VALIDATION_ERROR=1
+parse_ip_addr PGW_CLIENT_IPADDR_START || VALIDATION_ERROR=1
+parse_ip_addr PGW_CLIENT_IPADDR_END || VALIDATION_ERROR=1
+parse_ip_addr PGW_CLIENT_PRIMARY_DNS || VALIDATION_ERROR=1
+parse_ip_addr PGW_CLIENT_SECONDARY_DNS || VALIDATION_ERROR=1
+parse_ip_addr PGW_CLIENT_PRIMARY_NBNS || VALIDATION_ERROR=1
+parse_ip_addr PGW_CLIENT_SECONDARY_NBNS || VALIDATION_ERROR=1
 
 [ -n "$VALIDATION_ERROR" ] && exit_msg "Exiting due to missing configuration parameters"
 
@@ -75,3 +71,4 @@ done
 envsubst < /config/pgw-c-node.config.templ > /etc/ergw-gtp-c-node/ergw-gtp-c-node.config
 
 exec "$@"
+
--- pgw-u-node/docker-entrypoint.sh
+++ pgw-u-node/docker-entrypoint.sh
@@ -9,29 +9,29 @@ exit_msg () {
 # creates erlang notation of ip address without netmask
 # 192.168.10.1/24 -> 192,168,10,1
 ip_to_erlang() {
-    echo $1 | sed 's/\./,/g' - | sed 's/\/.*$//g' - | sed 's/.*/{\0}/g' -
+	echo $1 | sed 's/\./,/g' - | sed 's/\/.*$//g' - | sed 's/.*/{\0}/g' -
 }
 
 # creates erlang notation of network
 # 192.168.10.1/24 -> {192, 168, 10, 1}, 24
 net_to_erlang() {
-    echo $1 | sed -e 's/\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\/\([0-9]\{1,2\}\)/{\1, \2, \3, \4}, \5/' - | sed 's/.*/{\0}/g' -
+	echo $1 | sed -e 's/\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\/\([0-9]\{1,2\}\)/{\1, \2, \3, \4}, \5/' - | sed 's/.*/{\0}/g' -
 }
 
 
 parse_generic () {
-  #echo parsing ${1}=${!1}
-  [ -n "${!1}" ] || ( echo "env variable $1 is not set"; false )
+	#echo parsing ${1}=${!1}
+	[ -n "${!1}" ] || ( echo "env variable $1 is not set"; false )
 }
 
 parse_ip_net () {
-  #TODO: some more validation for IP networks
-  parse_generic $1 && eval export ${1}_ERL=\"$(net_to_erlang ${!1})\"
+	#TODO: some more validation for IP networks
+	parse_generic $1 && eval export ${1}_ERL=\"$(net_to_erlang ${!1})\"
 }
 
 parse_ip_addr () {
-  #TODO: some more validation for IP addresses
-  parse_generic $1 && eval export ${1}_ERL=\"$(ip_to_erlang ${!1})\"
+	#TODO: some more validation for IP addresses
+	parse_generic $1 && eval export ${1}_ERL=\"$(ip_to_erlang ${!1})\"
 }
 
 VALIDATION_ERROR=""
@@ -44,5 +44,8 @@ parse_ip_net PGW_CLIENT_IP_NET || VALIDATION_ERROR=1
 
 # create the config from template
 envsubst < /config/pgw-u-node.config.templ > /etc/ergw-gtp-u-node/ergw-gtp-u-node.config
+
+# unload gtp module as reset; will be reloaded on start of application
+#rmmod gtp
 
 exec "$@"
